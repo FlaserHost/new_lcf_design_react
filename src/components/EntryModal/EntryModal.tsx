e8f96bfa9c228a6156777bdeedbe430270da159c
Dmitry <flaserhost@gmail.com>
2023-08-28 17:53:03 +0300

    entryModal

--- src/components/EntryModal/EntryModal.tsx
+++ src/components/EntryModal/EntryModal.tsx
@@ -1,11 +1,12 @@
+import { useState } from "react";
 import { Helmet } from "react-helmet-async";
+import { ModalField } from "../ModalField/ModalField";
 import classNames from "classnames";
 import './styles/EntryModal.scss';
-import {useState} from "react";
 
 export const EntryModal = () => {
-    const [activeTab, setActiveTab] = useState({tab: 'auth', active: 'auth-active'});
-    const modalClasses = classNames('modal-tabs', activeTab.active);
+    const [activeTab, setActiveTab] = useState('auth');
+    const modalClasses = classNames('modal-tabs', `${activeTab}-active`);
 
     return (
         <>
@@ -16,8 +17,12 @@ export const EntryModal = () => {
                 <article className="entry-modal-body">
                     <div className={modalClasses}>
                         <div className="carriage"></div>
-                        <button className="entry-modal-body__tab auth-tab" type="button" onClick={() => setActiveTab({tab: 'auth', active: 'auth-active'})}>Авторизация</button>
-                        <button className="entry-modal-body__tab reg-tab" type="button" onClick={() => setActiveTab({tab: 'reg', active: 'reg-active'})}>Регистрация</button>
+                        <button className="entry-modal-body__tab auth-tab" type="button" onClick={() => setActiveTab('auth')}>Авторизация</button>
+                        <button className="entry-modal-body__tab reg-tab" type="button" onClick={() => setActiveTab('reg')}>Регистрация</button>
+                    </div>
+                    <div className="auth-body">
+                        <ModalField areaType='login' id='login' type='text' label="Логин" required={true} />
+                        <ModalField areaType='password' id='password' type='password' label="Пароль" required={true} />
                     </div>
                 </article>
             </section>
